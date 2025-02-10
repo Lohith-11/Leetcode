67a964ca8ef0126177fb03c192afcd2957f93589
Mareddy Lohith Reddy <116454212+Lohith-11@users.noreply.github.com>
2025-02-10 13:40:24 +0530

    Time: 4 ms (5.20%) | Memory: 41.6 MB (31.97%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        HashMap<Character,String> map=new HashMap<>();
+        HashSet<Character> c=new HashSet<>();
+        HashSet<String> d=new HashSet<>();
+        String[] x=s.split(" ");
+        int y=0;
+        if(pattern.length()!= x.length){
+            return false;
+        }
+        for(int i=0;i<pattern.length();i++){
+            char a=pattern.charAt(i);
+            c.add(a);
+            d.add(x[i]);
+            if(c.size()!=d.size()){
+                return false;
+            }
+            String ans=x[i];
+            if(map.containsKey(a)){
+                if(!map.get(a).equals(ans)){
+                    return false;
+                }
+                System.out.println(a);
+            }else{
+                if(map.containsValue(ans)){
+                    return false;
+                }
+                map.put(a,x[i]);
+            }
+        }
+        System.out.println(map);
+        return true;    
+    }
+}
