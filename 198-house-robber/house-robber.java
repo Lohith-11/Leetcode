2aa64825104827fbd8176927a71996f463d97577
Mareddy Lohith Reddy <116454212+Lohith-11@users.noreply.github.com>
2025-02-11 09:06:05 +0530

    Time: 2 ms (1.75%) | Memory: 40.9 MB (75.57%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int rob(int[] nums) {
+        int[] ans=new int[nums.length];
+        if(nums.length>2){
+        ans[0]=nums[0];
+        if(nums[0]>nums[1]){
+        ans[1]=nums[0];
+        }else{
+            ans[1]=nums[1];
+        }
+        }else{
+            Arrays.sort(nums);
+            return nums[nums.length-1];
+        }
+
+        for(int i=2;i<nums.length;i++){
+            int n=nums[i]+ans[i-2];
+            if(n>ans[i-1]){
+                ans[i]=n;
+            }else{
+                ans[i]=ans[i-1];
+            }
+        }
+        System.out.println(Arrays.toString(ans));
+
+        return ans[nums.length-1];
+    }
+}
