363f2d54eadd55b1190affcdacf21fc40322b114
Mareddy Lohith Reddy <116454212+Lohith-11@users.noreply.github.com>
2025-03-03 13:51:14 +0530

    Time: 2 ms (63.45%) | Memory: 42.7 MB (62.40%) - LeetSync

--- 415-add-strings/add-strings.java
+++ 415-add-strings/add-strings.java
@@ -1,8 +1,35 @@
 import java.math.*;
 class Solution {
     public String addStrings(String num1, String num2) {
-        BigInteger a=new BigInteger(num1);
-        BigInteger b=new BigInteger(num2);
-        return a.add(b).toString();
+        StringBuilder sb=new StringBuilder("");
+        int a=num1.length()-1;
+        int b=num2.length()-1;
+        int c=0;
+        while(a>=0 || b>=0){
+            int x;
+            int y;
+            if(a>=0){
+                x=num1.charAt(a)-'0';
+            }else{
+                x=0;
+            }
+            if(b>=0){
+                y=num2.charAt(b)-'0';
+            }else{
+                 y=0;
+            }
+            int sum=x+y+c;
+            c=sum/10;
+            sum%=10;
+            sb.append(sum);
+            sum=0;
+            a--;
+            b--;
+        }
+        if(c>0){
+            sb.append(c);
+        }
+        return sb.reverse().toString();
+
     }
 }
