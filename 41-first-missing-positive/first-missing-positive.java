67199aa4ccc9a2ac8ee27810f5f32b5b3bebb2d0
Mareddy Lohith Reddy <116454212+Lohith-11@users.noreply.github.com>
2025-03-04 11:11:55 +0530

    Time: 634 ms (5.85%) | Memory: 66.4 MB (6.03%) - LeetSync

---
+++ 41-first-missing-positive/first-missing-positive.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        Arrays.sort(nums);
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],1);
+        }
+        TreeMap<Integer,Integer> set=new TreeMap<>(map);
+        if(nums.length==1 && nums[0]!=1){
+            return 1;
+        }
+        if(nums[0]>1){
+            return 1;
+        }
+        if(!set.containsKey(1)){
+            return 1;
+        }
+        System.out.println(set);
+        for(int key : set.keySet()){
+            System.out.println(key);
+            
+            if(key>0 && !set.containsKey(key+1)){
+                return key+1;
+            }
+        }
+        return 1;
+    }
+}
