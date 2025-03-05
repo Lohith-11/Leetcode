670e48c0fc6153911f583acb091839c0de82e305
Mareddy Lohith Reddy <116454212+Lohith-11@users.noreply.github.com>
2025-03-05 19:15:26 +0530

    Time: 131 ms (15.07%) | Memory: 64.3 MB (12.66%) - LeetSync

--- 204-count-primes/count-primes.java
+++ 204-count-primes/count-primes.java
@@ -3,18 +3,14 @@ class Solution {
         if(n<2){
             return 0;
         }
-        int[] ans=new int[n];
-        for(int i=2;i*i<n;i++){
-            if(ans[i]==0){
-                for(int j=i*i;j<n;j+=i){
-                    ans[j]=1;
-                }
-            }
-        }
         int count=0;
+        int[] ans=new int[n];
         for(int i=2;i<n;i++){
             if(ans[i]==0){
                 count++;
+                for(int j=i*2;j<n;j+=i){
+                    ans[j]=1;
+                }
             }
         }
         return count;
