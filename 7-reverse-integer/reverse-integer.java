917f66557669e6230e979ac62c7f7ccc609dcacd
Mareddy Lohith Reddy <116454212+Lohith-11@users.noreply.github.com>
2025-03-05 19:20:15 +0530

    Time: 1 ms (83.72%) | Memory: 40.8 MB (84.44%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -3,11 +3,9 @@ class Solution {
         long ans=0;
         while(x!=0){
             int rem=x%10;
-            ans+=rem;
-            ans=ans*10;
+            ans=ans*10+rem;
             x=x/10;
         }
-        ans=ans/10;
         if(ans > Integer.MAX_VALUE || ans < Integer.MIN_VALUE){
             return 0;
         }
