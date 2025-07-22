e62f89e7ec80fa09c70d937ff5afefc1ad7d6515
Mareddy Lohith Reddy <116454212+Lohith-11@users.noreply.github.com>
2025-07-22 11:44:56 +0530

    Create 3483-AlternatingGroupsIi

---
+++ 3483-AlternatingGroupsIi/3483-AlternatingGroupsIi.java
@@ -0,0 +1,32 @@
+// Last updated: 7/22/2025, 11:45:43 AM
+class Solution {
+    public int numberOfAlternatingGroups(int[] colors, int k) {
+        List<Integer> arr = new ArrayList<>();
+        int n = colors.length;
+        for (int i = 0; i < n; i++) {
+            arr.add(colors[i]);
+        }
+        for (int i = 0; i < k - 1; i++) {
+            arr.add(colors[i]);
+        }
+        int ans = 0;
+        int i = 0;
+        int j = 1;
+        while (j < arr.size()) {
+            if ((arr.get(j) != arr.get(j - 1)) && j - i + 1 <= k) {
+                if(j-i+1==k){
+                    ans++;
+                    i++;
+                    j++;
+                }else{
+                    j++;
+                }
+            } else if (arr.get(j) == arr.get(j - 1)) {
+                i = j;
+                j++;
+            }
+        }
+        System.out.println(arr);
+        return ans;
+    }
+}
