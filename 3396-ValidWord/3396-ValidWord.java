a86ffa52744bceec17b9ffd0760b3e6b3c5770d2
Mareddy Lohith Reddy <116454212+Lohith-11@users.noreply.github.com>
2025-07-22 11:45:01 +0530

    Create 3396-ValidWord

---
+++ 3396-ValidWord/3396-ValidWord.java
@@ -0,0 +1,29 @@
+// Last updated: 7/22/2025, 11:45:49 AM
+class Solution {
+    public boolean isValid(String word) {
+        word=word.toUpperCase();
+        if(word.length()<3){
+            return false;
+        }
+        int consonant=0;
+        int vowel=0;
+        for(int i=0;i<word.length();i++){
+            char ch=word.charAt(i);
+            if(Character.isLetter(ch)){
+                if(ch=='A' || ch=='E' || ch=='I'||ch=='O'|| ch=='U'){
+                    vowel++;
+                }else{
+                    consonant++;
+                }
+            }
+            if(!Character.isLetter(ch) && !Character.isDigit(ch)){
+                return false;
+            }
+        }
+        if(vowel<1 || consonant<1){
+            return false;
+        }
+        return true;
+
+    }
+}
