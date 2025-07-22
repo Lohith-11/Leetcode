6553d95f84d982e674751b6b7e4d821bd0a00cca
Mareddy Lohith Reddy <116454212+Lohith-11@users.noreply.github.com>
2025-07-22 11:44:53 +0530

    Create 3600-FindTheKThCharacterInStringGameI

---
+++ 3600-FindTheKThCharacterInStringGameI/3600-FindTheKThCharacterInStringGameI.java
@@ -0,0 +1,20 @@
+// Last updated: 7/22/2025, 11:45:41 AM
+class Solution {
+    public char kthCharacter(int k) {
+        StringBuilder ans=new StringBuilder("a");
+        while(ans.length()<k){
+            StringBuilder sb=new StringBuilder("");
+            for(int i=0;i<ans.length();i++){
+                char c=ans.charAt(i);
+                if(c=='z'){
+                    sb.append('a');
+                }else{
+                    sb.append((char) (c+1));
+                }
+            }
+            ans.append(sb);
+        }
+        System.out.println(ans);
+        return ans.charAt(k-1);
+    }
+}
