28899384b10f446660a034d958b8c667c3ab788d
Mareddy Lohith Reddy <116454212+Lohith-11@users.noreply.github.com>
2025-07-22 11:44:46 +0530

    Create 3753-MaximumDifferenceBetweenEvenAndOddFrequencyI

---
+++ 3753-MaximumDifferenceBetweenEvenAndOddFrequencyI/3753-MaximumDifferenceBetweenEvenAndOddFrequencyI.java
@@ -0,0 +1,25 @@
+// Last updated: 7/22/2025, 11:45:34 AM
+class Solution {
+    public int maxDifference(String s) {
+        HashMap <Character,Integer> map= new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(map.containsKey(c)){
+                map.put(c,map.get(c)+1);
+            }else{
+                map.put(c,1);
+            }
+        }
+        int odd=0;
+        int even=101;
+        for(char key:map.keySet()){
+            int x=map.get(key);
+            if(x%2!=0 && x>=odd){
+                odd=x;
+            }else if(x%2==0 && x<=even){
+                even=x;
+            }
+        }
+        return odd-even;
+    }
+}
