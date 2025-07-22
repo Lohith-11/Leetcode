de706fdadf1525dd1e091c00b4cb9866babdb836
Mareddy Lohith Reddy <116454212+Lohith-11@users.noreply.github.com>
2025-07-22 11:45:02 +0530

    Create 3434-FindTheNumberOfDistinctColorsAmongTheBalls

---
+++ 3434-FindTheNumberOfDistinctColorsAmongTheBalls/3434-FindTheNumberOfDistinctColorsAmongTheBalls.java
@@ -0,0 +1,32 @@
+// Last updated: 7/22/2025, 11:45:50 AM
+class Solution {
+    public int[] queryResults(int limit, int[][] queries) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        HashMap<Integer,Integer> color=new HashMap<>();
+        int[] arr=new int[queries.length];
+        for(int i=0;i<queries.length;i++){
+            int x=queries[i][0];
+            int y=queries[i][1];
+            if(map.containsKey(x)){
+                int z=map.get(x);
+                if(z==y){
+                    arr[i]=color.size();
+                    continue;
+                }
+                if(color.get(z) == 1){
+                    color.remove(z);
+                }else{
+                color.put(z,color.get(z)-1);
+                }
+            }
+            map.put(x,y);
+            if(color.containsKey(y)){
+                color.put(y,color.get(y)+1);
+            }else{
+                color.put(y,1);
+            }
+            arr[i]=color.size();
+        }
+        return arr;
+    }
+}
