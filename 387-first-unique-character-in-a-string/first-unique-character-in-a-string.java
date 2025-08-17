a606645dd736f2ca4b8c53a9b4d160a7c21cf726
Mareddy Lohith Reddy <116454212+Lohith-11@users.noreply.github.com>
2025-08-17 16:19:21 +0530

    Time: 16 ms (54.66%) | Memory: 46.8 MB (6.65%) - LeetSync

---
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] freq=new int[26];
+        Queue<Character> q=new LinkedList<>();
+        int ans=-1;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            q.add(ch);
+            freq[ch-'a']++;
+        }
+        while(!q.isEmpty() && freq[q.peek()-'a']>1){
+            q.remove();
+        }
+        if(q.isEmpty()){
+            return ans;
+        }else{
+            ans=s.indexOf(q.peek());
+        }
+        return ans;
+    }
+}
